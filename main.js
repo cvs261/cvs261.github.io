94661e05762086d21e2a88666be3c0ac136dee87
cvs261 <vconstantin261@gmail.com>
2024-03-06 10:29:35 +0200

    Commited

--- main.js
+++ main.js
@@ -141,7 +141,34 @@ function receive(data){
     log(data, 'in');
 }
 
+function writeToCharacteristic(characteristic, data) {
+    characteristic.writeValue(new TextEncoder().encode(data));
+}
+
 // Send data to the connected device
 function send(data) {
-    //
+    data = String(data);
+
+    if (!data || !characteristicCache) {
+        return;
+    }
+
+    data += '\n';
+
+    if (data.length > 20) {
+        let chunks = data.match(/(.|[\r\n]){1,20}/g);
+
+        writeToCharacteristic(characteristicCache, chunks[0]);
+
+        for (let i = 1; i < chunks.length; i++) {
+            setTimeout(() => {
+                writeToCharacteristic(characteristicCache, chunks[i]);
+            }, i * 100);
+        }
+    }
+    else {
+        writeToCharacteristic(characteristicCache, data);
+    }
+
+    log(data, 'out');
 }
